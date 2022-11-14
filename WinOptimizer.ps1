5aa14d814efc979306f792ae562b3615083b6820
xfury94 <lestarangel@gmail.com>
2022-11-14 22:15:45 +0000

    Added O&O with Chris's Custom Configuration.

--- WinOptimizer.ps1
+++ WinOptimizer.ps1
@@ -599,6 +599,14 @@ $IpList = @(
                 Write-Host "Blocking $Ip."
             }
 
+Write-Host "Running O&O with Chris custom config."
+            If (!(Test-Path .\ooshutup10.cfg)) {
+                Write-Host "Running O&O Shutup with Recommended Settings"
+                curl.exe -s "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg" -o ooshutup10.cfg
+                curl.exe -s "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -o OOSU10.exe
+            }
+            ./OOSU10.exe ooshutup10.cfg /quiet
+
 Write-Host "Removing Dependencies."
             foreach ($Dep in $Deps){
                 Write-Host "Removing $Dep."
